2a5d58020539d80991623ec6331c638f0f45a93c
Nathaniel <nathanielmorales01@gmail.com>
2020-01-23 15:39:20 +1100

    mongoose connection, views and basic routing

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv").config({ encoding: "latin1" });
+const expressLayouts = require("express-ejs-layouts");
+// const dotenv = require("dotenv").config({ encoding: "latin1" }
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+// Router Routes
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+// Mongoose Connection
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 });
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
-app.listen(3000, () => {
+// Routers
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000, () => {
   console.log(`You are now connected on Port ${process.env.PORT}`);
 });
